5523b5f781ae88117556dab27149d7e3374e3670
acpride <acpride@gmail.com>
2015-10-20 13:59:20 +0200

    Add rendering scales to SVG, JPG and PNG

--- src/com/ride/ukescales/test/UkeScalesTester.java
+++ src/com/ride/ukescales/test/UkeScalesTester.java
@@ -1,50 +1,108 @@
 package com.ride.ukescales.test;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Paths;
+import java.util.List;
 
 import org.apache.batik.transcoder.TranscoderException;
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.TranscodingHints;
 import org.apache.batik.transcoder.image.JPEGTranscoder;
+import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.apache.xml.serialize.OutputFormat;
 import org.apache.xml.serialize.XMLSerializer;
 import org.w3c.dom.Document;
 
+import com.ride.ukescales.Note;
 import com.ride.ukescales.Pitch;
 import com.ride.ukescales.Scale;
+import com.ride.ukescales.Scale.ScaleType;
 import com.ride.ukescales.render.Fretboard;
 import com.ride.ukescales.render.ScaleRenderer;
 
 public class UkeScalesTester {
 
+	private static final String BASE_PATH_MAJOR = "c://temp//major//";
+	private static final String BASE_PATH_MINOR = "c://temp//minor//";
 	/**
 	 * @param args
 	 * @throws Exception
 	 */
-	public static void main(String[] args) throws Exception {
-
+	public static void main(String[] args) throws Exception {		
+		
+		/*
+		Scale scale2 = new Scale("C");
+		List<Note> myScale = scale2.getScale(ScaleType.MAJOR_SCALE);
+		
 		ScaleRenderer sr = new ScaleRenderer();
-		Document svg = sr.fretboardSVGRender(new Fretboard());
+		Fretboard fb = new Fretboard();
+		Document svg = sr.fretboardSVGRender(fb);
+		svg = sr.renderScale(svg, fb, myScale, ScaleType.MAJOR_SCALE);
+		renderToPNG(svg, "c://temp//testC.png");
+		*/
+		
 
+		
+		//Scale scale = new Scale("Gb");//ERROR!!! tambien en la relativa  Eb menor
+		//Scale scale = new Scale("Cb");//MAJOR ERROR!!!
+		//Scale scale = new Scale("Eb");
+		//scale.parse(Scale.ScaleType.MINOR_SCALE);
+		//Scale scale = new Scale("C");
+		//scale.parse(ScaleType.MAJOR_SCALE);
+		//scale.relativeMinor();
+		//scale.relativeMajor();
+		
+		
 		System.out.println("---MAJOR SCALES---");
 		for(String pitch : Scale.getCircleoffifths()){
 			Scale scale = new Scale(pitch);
-			scale.parse(Scale.ScaleType.MAJOR_SCALE);
+			//scale.parse(Scale.ScaleType.MAJOR_SCALE);
+			List<Note> myScale = scale.getScale(ScaleType.MAJOR_SCALE);
+			ScaleRenderer sr = new ScaleRenderer();
+			Fretboard fb = new Fretboard();
+			Document svg = sr.fretboardSVGRender(fb);
+			svg = sr.renderScale(svg, fb, myScale, ScaleType.MAJOR_SCALE);
+			
+			String outputSVG = BASE_PATH_MAJOR + getOutputFileName(scale.getKey(),"svg");
+			renderToSVG(svg, outputSVG);
+			String outputJPG = BASE_PATH_MAJOR + getOutputFileName(scale.getKey(),"jpg");
+			renderToJPG(svg, outputJPG);
+			String outputPNG = BASE_PATH_MAJOR + getOutputFileName(scale.getKey(),"png");
+			renderToPNG(svg, outputPNG);			
 		}
 		
-		System.out.println("\n---MINOR SCALES---");
+		System.out.println("---MINOR SCALES---");
 		for(String pitch : Scale.getCircleoffifths()){
 			Scale scale = new Scale(pitch);
-			scale.parse(Scale.ScaleType.MINOR_SCALE);
+			String relativePitch = scale.relativeMinor();
+			scale = new Scale(relativePitch);
+			List<Note> myScale = scale.getScale(ScaleType.MINOR_SCALE);
+			ScaleRenderer sr = new ScaleRenderer();
+			Fretboard fb = new Fretboard();
+			Document svg = sr.fretboardSVGRender(fb);
+			svg = sr.renderScale(svg, fb, myScale, ScaleType.MINOR_SCALE);
+			
+			String outputSVG = BASE_PATH_MINOR + getOutputFileName(scale.getKey(),"svg");
+			renderToSVG(svg, outputSVG);
+			String outputJPG = BASE_PATH_MINOR + getOutputFileName(scale.getKey(),"jpg");
+			renderToJPG(svg, outputJPG);
+			String outputPNG = BASE_PATH_MINOR + getOutputFileName(scale.getKey(),"png");
+			renderToPNG(svg, outputPNG);	
+
 		}
-				
+	}
+	
+	private static void renderToSVG(Document svg, String output){
+		//render svg file
 		OutputFormat format = new OutputFormat(svg);
 		format.setIndenting(true);
 		OutputStream svgOutStream;
 		try {
-			svgOutStream = new FileOutputStream("c://temp//out.svg");
+			svgOutStream = new FileOutputStream(output);
 
 			XMLSerializer serializer = new XMLSerializer(svgOutStream, format);
 
@@ -54,6 +112,9 @@ public class UkeScalesTester {
 			e1.printStackTrace();
 		}
 
+	}
+	
+	private static void renderToJPG(Document svg, String output){
 		// Create a JPEGTranscoder and set its quality hint.
 		JPEGTranscoder t = new JPEGTranscoder();
 		t.addTranscodingHint(JPEGTranscoder.KEY_QUALITY, new Float(.8));
@@ -61,11 +122,11 @@ public class UkeScalesTester {
 		// Set the transcoder input and output.
 		try {
 			TranscoderInput input = new TranscoderInput(svg);
-			OutputStream ostream = new FileOutputStream("c://temp//out.jpg");
-			TranscoderOutput output = new TranscoderOutput(ostream);
+			OutputStream ostream = new FileOutputStream(output);
+			TranscoderOutput outputJPG = new TranscoderOutput(ostream);
 
 			// Perform the transcoding.
-			t.transcode(input, output);
+			t.transcode(input, outputJPG);
 			ostream.flush();
 			ostream.close();
 		} catch (TranscoderException e) {
@@ -75,6 +136,47 @@ public class UkeScalesTester {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private static void renderToPNG(Document svg, String output){
+		
+		try {
+			TranscoderInput input_svg_image = new TranscoderInput(svg);
+			OutputStream png_ostream = new FileOutputStream(output);
+			TranscoderOutput output_png_image = new TranscoderOutput(png_ostream);
+			
+			PNGTranscoder png = new PNGTranscoder();
+			png.addTranscodingHint(PNGTranscoder.KEY_WIDTH, new Float(1000));
+			png.addTranscodingHint(PNGTranscoder.KEY_GAMMA, new Float(0));
+			png.transcode(input_svg_image, output_png_image);
+			
+			png_ostream.flush();
+			png_ostream.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TranscoderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}   
+			
+	}
+	
+	private static String getOutputFileName(String pitch, String format){
+		
+		if(pitch.length()==1){
+			return pitch + "." + format;
+		}else{
+			if(pitch.substring(1, 2).equals("#")){
+				return pitch.substring(0, 1) + "sharp." + format;
+			}else{
+				return pitch.substring(0, 1) + "flat." + format;
+			}
+		}		
 	}
 
 }
